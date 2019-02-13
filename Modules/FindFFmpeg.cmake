380dc6442491f81e20ee71f42fd291ac898c2734
Zhaosheng Pan <brglng@gmail.com>
2019-02-13 10:33:08 +0800

    Fix FFmpeg include directories

--- Modules/FindFFmpeg.cmake
+++ Modules/FindFFmpeg.cmake
@@ -3,15 +3,15 @@ foreach(comp ${FFmpeg_FIND_COMPONENTS})
     message(FATAL_ERROR "${comp} is not a FFmpeg component name")
   endif()
 
-  find_path(FFmpeg_${comp}_INCLUDE_DIR lib${comp}/${comp}.h ${FFmpeg_ROOT})
+  find_path(FFmpeg_${comp}_INCLUDE_DIRS lib${comp}/${comp}.h ${FFmpeg_ROOT})
 
-  if(NOT (${FFmpeg_${comp}_INCLUDE_DIR} STREQUAL ""))
-    list(APPEND FFmpeg_INCLUDE_DIRS ${FFmpeg_${comp}_INCLUDE_DIR})
+  if(NOT (${FFmpeg_${comp}_INCLUDE_DIRS} STREQUAL ""))
+    list(APPEND FFmpeg_INCLUDE_DIRS ${FFmpeg_${comp}_INCLUDE_DIRS})
   endif()
 
   find_library(FFmpeg_${comp}_LIBRARIES ${comp} ${FFmpeg_ROOT})
 
-  if(NOT ((${FFmpeg_${comp}_INCLUDE_DIR} STREQUAL "") OR (${FFmpeg_${comp}_LIBRARIES} STREQUAL "")))
+  if(NOT ((${FFmpeg_${comp}_INCLUDE_DIRS} STREQUAL "") OR (${FFmpeg_${comp}_LIBRARIES} STREQUAL "")))
     set(FFmpeg_${comp}_FOUND True)
     list(APPEND FFmpeg_LIBRARIES ${FFmpeg_${comp}_LIBRARIES})
   endif()
@@ -23,13 +23,13 @@ find_package_handle_standard_args(FFmpeg REQUIRED_VARS FFmpeg_LIBRARIES FFmpeg_I
 
 mark_as_advanced(
   FFmpeg_INCLUDE_DIRS
-  FFmpeg_avformat_INCLUDE_DIR
-  FFmpeg_avcodec_INCLUDE_DIR
-  FFmpeg_avutil_INCLUDE_DIR
-  FFmpeg_swscale_INCLUDE_DIR
-  FFmpeg_avdevice_INCLUDE_DIR
-  FFmpeg_avfilter_INCLUDE_DIR
-  FFmpeg_swresample_INCLUDE_DIR
+  FFmpeg_avformat_INCLUDE_DIRS
+  FFmpeg_avcodec_INCLUDE_DIRS
+  FFmpeg_avutil_INCLUDE_DIRS
+  FFmpeg_swscale_INCLUDE_DIRS
+  FFmpeg_avdevice_INCLUDE_DIRS
+  FFmpeg_avfilter_INCLUDE_DIRS
+  FFmpeg_swresample_INCLUDE_DIRS
   FFmpeg_LIBRARIES
   FFmpeg_avformat_LIBRARIES
   FFmpeg_avcodec_LIBRARRIES
